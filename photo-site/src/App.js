7518d340c3285e49c96a3abad5a43790dd01900c
Clinelly <swall289@gmail.com>
2023-04-18 15:51:20 +0000

    Add setState components.

--- photo-site/src/App.js
+++ photo-site/src/App.js
@@ -2,12 +2,18 @@ import logo from './logo.svg';
 import './App.css';
 import FunctionalGreeting from './components/FunctionalGreeting';
 import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+// import StatefulGreeting from './components/StatefulGreeting';
+// import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 function App() {
   return (
     <div className="App">
         <FunctionalGreeting />
         <FunctionalGreetingWithProps greeting="Nice to meet you." name="Stuart." age="You are 34 years old." />
+        {/* <StatefulGreeting name="Megan" greeting="I'm a Stateful class component!"/> */}
+        {/* <StatefulGreetingWithCallback name="Dorito" greeting="You are fluffy."/> */}
+        <StatefulGreetingWithPrevState />
     </div>
   );
 }
